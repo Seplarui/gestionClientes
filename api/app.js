a4e9ab4eb83ba19a8ccbafc9ee7e0ebd714b657c
Sebas <seplarui@gmail.com>
2020-05-28 22:00:08 +0200

    begining

---
+++ api/app.js
@@ -0,0 +1,21 @@
+'use strict'
+const express = require('express')
+const bodyParser = require('body-parser')
+//const router = require('./routes')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
+
+//app.use('', router)
+
+module.exports = app;
