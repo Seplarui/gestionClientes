7670af2cf04601c6938c128fc5689cb4584995f0
Sebas <seplarui@gmail.com>
2020-05-31 21:05:56 +0200

    getLogin

--- front/front/src/app/services/data-api.service.ts
+++ front/front/src/app/services/data-api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLogin() {
+    const urlApi = 'localhost:3001/loginuser';
+
+    return this.http.get(urlApi);
+  }
 }
